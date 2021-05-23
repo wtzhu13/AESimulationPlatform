c08c3bcd5a3a9b83e84e7d06dafca24b4c033897
wtzhu13 <wtzhu_13@163.com>
2021-05-23 18:43:47 +0800

    update UI for some parameters

--- src/nextFrame.m
+++ src/nextFrame.m
@@ -1,3 +1,8 @@
+%% --------------------------------
+%% author:wtzhu
+%% date: 20210523
+%% fuction: я╟уробр╩ж║м╪ф╛
+%% --------------------------------
 function nextFrameName = nextFrame()
     nextFrameName = '2.jpg';
 end
