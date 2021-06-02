78faa1d98aef61ade454999c204bba47cc34d0e2
Fred <fred@mysher.com>
2021-06-02 23:12:57 +0800

    collect image test on PC OK

--- dataCollection/collectImages.py
+++ dataCollection/collectImages.py
@@ -16,7 +16,12 @@ author      |   Date    |   Describe
 wtzhu       | 20210602  |
 ------------------------------------------    
 """
+import os
+import shutil
 import pyautogui as pag
+from time import sleep
+from time import time
+pag.PAUSE = 0.5
 
 
 def auto_save_images():
@@ -24,27 +29,42 @@ def auto_save_images():
     save images auto bu use pyautogui
     :return:
     """
+    # to IQTool UI
+    pag.click(1137, 172)
     # save image from 300-30000, step is 10
+    for us in range(300, 30301, 100):
+        # set shutter by click shutter range
+        pag.click(830, 590, clicks=2)
+        # set shutter
+        pag.write(str(us))
+        # write parameter into board
+        pag.press("enter")
+        sleep(1)
+        # save image
+        pag.click(1350, 230)
+        # format name
+        format_file_name(str(us))
 
-    # set shutter by click shutter range
 
-    # write parameter into board
-
-    # save image by click jpg button
-    pass
-
-
-def format_file_name():
+def format_file_name(us):
     """
     rename the image file using the required format
     :return:
     """
     # move image to the required path
-
-    # rename the image by using the format that required
-    pass
+    path = "E:\\Mysher\\Sigmastar\\SS333\\tools\\apitool\\Image"
+    file_type = ".jpg"
+    for index, value in enumerate(os.listdir(path)):
+        if value.endswith(file_type):
+            print(value)
+            os.rename(path + "\\" + value, path + "\\" + "FN_18_GAIN_1_ET_{}.jpg".format(us))
+            shutil.move(path + "\\" + "FN_18_GAIN_1_ET_{}.jpg".format(us), path + "\\data")
 
 
 if __name__ == '__main__':
+    start = time()
     auto_save_images()
-    format_file_name()
+    end = time()
+    print(f"use time: {end - start}s")
+    # format_file_name()
+
